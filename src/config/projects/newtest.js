5f4f91e122d5ed25caf143c2cd977e3007af0805
rishabhdelta4infotech <rishabh.delta4infotech@gmail.com>
2025-03-13 17:06:08 +0530

    sdffsdfdsf

--- src/config/projects/newtest.js
+++ src/config/projects/newtest.js
@@ -7,7 +7,7 @@
 
 module.exports = {
     // Project name (displayed in notifications)
-    name: 'New Test Product 23232323',
+    name: 'New Test Product 232',
     
     // Repository full name (owner/repo)
     repository: 'rishabhdelta4infotech/testProduct',
